a8d8085f2a4b25310a5d53bdf2cf224bb618e1d6
Gonçalo Pinto <gongaspinto03@gmail.com>
2024-03-17 22:57:40 +0000

    tp4-1

--- tp4/MyTangram.js
+++ tp4/MyTangram.js
@@ -1,4 +1,4 @@
-import { CGFobject, CGFappearance } from "../lib/CGF.js";
+import { CGFobject, CGFappearance } from '../lib/CGF.js';
 import { MyDiamond } from "./MyDiamond.js";
 import { MyParallelogram } from "./MyParallelogram.js";
 import { MyTriangle } from "./MyTriangle.js";
@@ -13,11 +13,12 @@ export class MyTangram extends CGFobject {
     constructor(scene) {
         super(scene);
         this.diamond = new MyDiamond(this.scene);
-        this.triangle = new MyTriangle(this.scene);
-        this.triangleSmall = new MyTriangleSmall(this.scene);
-        this.triangleBig = new MyTriangleBig(this.scene);
+        this.trianglePink = new MyTriangle(this.scene);
+        this.triangleBlue = new MyTriangleBig(this.scene, [0, 0, 1, 0, 0.5, 0.5, 0, 0, 1, 0, 0.5, 0.5]);
+        this.triangleOrange = new MyTriangleBig(this.scene, [1, 0, 1, 1, 0.5, 0.5, 1, 0, 1, 1, 0.5, 0.5]);
+        this.trianglePurple = new MyTriangleSmall(this.scene, [0, 0, 0, 0.5, 0.25, 0.25, 0, 0, 0, 0.5, 0.25, 0.25]);
+        this.triangleRed = new MyTriangleSmall(this.scene, [0.25, 0.75, 0.75, 0.75, 0.5, 0.5, 0.25, 0.75, 0.75, 0.75, 0.5, 0.5]);
         this.parallelogram = new MyParallelogram(this.scene);
-        this.scaleFactor = 1;
         this.initMaterials();
     }
 
@@ -60,8 +61,8 @@ export class MyTangram extends CGFobject {
 
         // this.parallelogram 
         this.parallelogramMaterial = new CGFappearance(this.scene);
-        this.parallelogramMaterial.setAmbient(255/255, 255/255, 0, 1.0);
-        this.parallelogramMaterial.setDiffuse(1, 0, 0, 0);
+        this.parallelogramMaterial.setAmbient(255/255, 153/255, 255/255, 1.0);
+        this.parallelogramMaterial.setDiffuse(1, 1, 0, 0);
         this.parallelogramMaterial.setSpecular(0.9, 0.9, 0.9, 1.0);
         this.parallelogramMaterial.setShininess(10.0);        
 
@@ -71,9 +72,18 @@ export class MyTangram extends CGFobject {
         this.trianglePinkMaterial.setDiffuse(1, 1, 0, 0);
         this.trianglePinkMaterial.setSpecular(0.9, 0.9, 0.9, 1.0);
         this.trianglePinkMaterial.setShininess(10.0);
+        
+        // Tangram texture
+        this.texture = new CGFappearance(this.scene);
+        this.texture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.texture.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.texture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.texture.setShininess(10.0);
+        this.texture.loadTexture('images/tangram.png');
     }
-    
+
     display() {
+
         this.scene.pushMatrix();
         let translationMatrix = [
             1, 0, 0, 0,
@@ -83,17 +93,19 @@ export class MyTangram extends CGFobject {
         ];
 
         this.scene.multMatrix(translationMatrix)
+        this.scene.translate(0.1 , -0.9, 0);
+        this.diamondMaterial.apply();
         this.texture.apply()
         this.diamond.display()
         this.scene.popMatrix()
 
-        // Draw blue big triangle
-        // NOTE: first we rotate and then translate, but in code we do the reverse reasoning
+        //Blue triangle
         this.scene.pushMatrix();
         this.scene.translate(-Math.sqrt(2), -Math.sqrt(2), 0);
         this.scene.rotate(-135*Math.PI/180,0,0,1);
         this.triangleBlueBigMaterial.apply();
-        this.triangleBig.display();
+        this.texture.apply();
+        this.triangleBlue.display();
         this.scene.popMatrix();
 
         // Draw orange big triangle
@@ -101,17 +113,19 @@ export class MyTangram extends CGFobject {
         this.scene.translate(-2, 0, 0);
         this.scene.rotate(180*Math.PI/180,0,0,1);
         this.triangleOrangeBigMaterial.apply();
-        this.triangleBig.display();
+        this.texture.apply();
+        this.triangleOrange.display();
         this.scene.popMatrix();
 
-        // Draw red small triangle 
+        // Draw red small triangle
         this.scene.pushMatrix();
         var x_mov = 1 + 0.5;      // just to make it more readable (triangle height + slight difference)
         var y_mov = -0.5;
         this.scene.translate(x_mov,y_mov,0);
         this.scene.rotate(Math.PI/2,0,0,1);
         this.triangleRedSmallMaterial.apply();
-        this.triangleSmall.display();
+        this.texture.apply();
+        this.triangleRed.display();
         this.scene.popMatrix();
 
         // Draw purple small triangle 
@@ -120,25 +134,28 @@ export class MyTangram extends CGFobject {
         var yi_mov = 3 + 1/2 - Math.sqrt(2)/2;
         this.scene.translate(xi_mov,yi_mov,0);
         this.trianglePurpleSmallMaterial.apply();
+        this.texture.apply();
         this.scene.rotate(3*Math.PI/4,0,0,1);
-        this.triangleSmall.display();
+        this.trianglePurple.display();
         this.scene.popMatrix();
-        
+
         // Draw parallelogram
         this.scene.pushMatrix();
         this.scene.translate(-0.5,0.5,0);
         this.scene.rotate(-Math.PI/2,0,0,1);  // colocar na vertical
         this.scene.rotate(Math.PI,0,1,0);     // inverter
         this.parallelogramMaterial.apply();
+        this.texture.apply();
         this.parallelogram.display();
         this.scene.popMatrix();
-        
+
         // Draw pink triangle
         this.scene.pushMatrix();
         this.scene.translate(0,-Math.sqrt(2),0);
         this.scene.rotate(-225*Math.PI/180,0,0,1);
         this.trianglePinkMaterial.apply();
-        this.triangle.display();
+        this.texture.apply();
+        this.trianglePink.display();
         this.scene.popMatrix();
     }
 
--- tp4/README.md
+++ tp4/README.md
@@ -1,1 +1,15 @@
-# [COPY FROM PREVIOUS TP AND ADJUST]
+# CG 2023/2024
+
+## Group T05G05
+
+## TP 4 Notes
+
+- 
+
+### Exercise 1 - Applying textures to Tangram
+
+![Screenshot 1](screenshots/cg-t05g05-tp4-1.png)
+
+### Exercise 2 - Applying textures to a cube composed of planes
+
+![Screenshot 2](screenshots/cg-t05g05-tp4-2.png)
